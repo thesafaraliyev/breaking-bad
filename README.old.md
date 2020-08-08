f312fa67c2cb2151f897dacc3cef985e2cb744d9
Elshan Safaraliyev <elshansafaraliyev@gmail.com>
2020-08-08 13:19:27 +0400

    Initial commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# breaking-bad
+Simple Breaking Bad cast site with React and The Breaking Bad API
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      Say my name!
+    </div>
+  );
+}
+
+export default App;
